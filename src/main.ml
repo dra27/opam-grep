1b86a36a6bebe44e8a36ebca8f603bc405b4b1ac
Kate <kit.ty.kate@disroot.org>
2021-09-11 19:17:45 +0100

    Make the version number accessible through --version

--- src/main.ml
+++ src/main.ml
@@ -24,15 +24,13 @@ let main_regexp =
   Arg.pos ~rev:true 0 (Arg.some Arg.string) None &
   Arg.info [] ~docv:"REGEXP" ~doc
 
-let version = "TODO"
-
 let cmd =
   let doc = "greps anything in the sources of the latest version of every opam packages" in
   let sdocs = Manpage.s_common_options in
   let exits = Term.default_exits in
   let man = [] in (* TODO *)
   Term.ret (Term.const main $ regexp_arg $ main_regexp),
-  Term.info "opam-grep" ~version ~doc ~sdocs ~exits ~man
+  Term.info "opam-grep" ~version:Config.version ~doc ~sdocs ~exits ~man
 
 let () =
   Term.exit @@ match Term.eval cmd with
