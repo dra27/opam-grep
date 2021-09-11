937fd65ae8ebfc588ec0a7bf6fd5ec164e49e0f5
Kate <kit.ty.kate@disroot.org>
2021-09-11 18:16:54 +0100

    Simpify the handling of command lines

--- src/grep.ml
+++ src/grep.ml
@@ -4,6 +4,7 @@ module Dir = Bos.OS.Dir
 module Path = Bos.OS.Path
 
 let ( // ) = Fpath.( / )
+let ( % ) = Cmd.( % )
 let result = Rresult.R.failwith_error_msg
 
 let list_split_bunch n l =
@@ -23,15 +24,6 @@ let successful_cmd ~msg = function
   | (x, (_, `Exited 0)) -> x
   | (_, (_, (`Exited _ | `Signaled _))) -> failwith msg
 
-module Commands = struct
-  let opam_list = Cmd.(v "opam" % "list" % "-A" % "-s" % "--color=never")
-  let opam_show pkgs = Cmd.(v "opam" % "show" % "--color=never" % "-f" % "package" %% of_list pkgs)
-  let opam_source ~path pkg = Cmd.(v "opam" % "source" % "--dir" % Fpath.to_string path % pkg)
-  let ripgrep ~regexp ~dir = Cmd.(v "rg" % "--binary" % "-qsr" % "-e" % regexp % Fpath.to_string dir)
-  let ugrep ~regexp ~dir = Cmd.(v "ugrep" % "--binary" % "-qsr" % "-e" % regexp % Fpath.to_string dir)
-  let grep ~regexp ~dir = Cmd.(v "grep" % "--binary" % "-qsr" % "-e" % regexp % Fpath.to_string dir)
-end
-
 let dst () =
   let cachedir =
     match Sys.getenv_opt "XDG_CACHE_HOME" with
@@ -46,12 +38,12 @@ let dst () =
 let sync ~dst =
   let _exists : bool = result (Dir.create ~path:true dst) in
   let pkgs_bunch =
-    result (Exec.out_lines (Exec.run_out Commands.opam_list)) |>
+    result (Exec.out_lines (Exec.run_out Cmd.(v "opam" % "list" % "-A" % "-s" % "--color=never"))) |>
     successful_cmd ~msg:"opam list failed" |>
-    list_split_bunch 100
+    list_split_bunch 255 (* Smallest value of MAX_ARG: https://www.in-ulm.de/~mascheck/various/argmax/ *)
   in
   let opam_show pkgs =
-    result (Exec.out_lines (Exec.run_out (Commands.opam_show pkgs))) |>
+    result (Exec.out_lines (Exec.run_out Cmd.(v "opam" % "show" % "--color=never" % "-f" % "package" %% of_list pkgs))) |>
     successful_cmd ~msg:"opam grep failed"
   in
   List.map opam_show pkgs_bunch |> List.concat |> List.sort_uniq String.compare
@@ -61,22 +53,20 @@ let check ~dst pkg =
   let pkgdir = dst // pkg in
   if not (result (Dir.exists pkgdir)) then begin
     result (Dir.delete ~recurse:true tmpdir);
-    let _ : (unit, _) result = Exec.success (Exec.out_null (Exec.run_out ~err:Exec.err_null (Commands.opam_source ~path:tmpdir pkg))) in
+    let _ : (unit, _) result = Exec.success (Exec.out_null (Exec.run_out ~err:Exec.err_null Cmd.(v "opam" % "source" % "--dir" % Fpath.to_string tmpdir % pkg))) in
     result (Path.move tmpdir pkgdir)
   end
 
 let get_grep_cmd () =
-  (* TODO: Avoid using dummy arguments *)
-  let dir = result (Dir.current ()) in
-  let ripgrep = Commands.ripgrep ~regexp:"" ~dir in
-  let ugrep = Commands.ugrep ~regexp:"" ~dir in
-  let grep = Commands.grep ~regexp:"" ~dir in
+  let ripgrep = Cmd.v "rg" in
+  let ugrep = Cmd.v "ugrep" in
+  let grep = Cmd.v "grep" in
   if result (Exec.exists ripgrep) then
-    Commands.ripgrep
+    ripgrep
   else if result (Exec.exists ugrep) then
-    Commands.ugrep
+    ugrep
   else if result (Exec.exists grep) then
-    Commands.grep
+    grep
   else
     failwith "Could not find any grep command"
 
@@ -92,7 +82,7 @@ let search ~regexp =
     List.iter begin fun pkg ->
       progress 1;
       check ~dst pkg;
-      match Exec.run (grep ~regexp ~dir:(dst // pkg)) with
+      match Exec.run (grep % "--binary" % "-qsr" % "-e" % regexp % Fpath.to_string (dst // pkg)) with
       | Ok () ->
           let pkg = List.hd (String.split_on_char '.' pkg) in
           Progress.interject_with begin fun () ->
